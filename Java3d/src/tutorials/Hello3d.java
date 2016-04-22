e55d8ea70ae5aef68c5ef1a6495a0352558e66a8
lemon-ada <catherine.ygr@gmail.com>
2016-04-22 15:15:57 -0400

    testing edit

--- Java3d/src/tutorials/Hello3d.java
+++ Java3d/src/tutorials/Hello3d.java
@@ -2,18 +2,12 @@ package tutorials;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import com.sun.j3d.utils.geometry.ColorCube;
 import javax.media.j3d.BranchGroup;
-import javax.media.j3d.TransformGroup;
-	
 public class Hello3d {
 		public  Hello3d() {
 			SimpleUniverse universe = new SimpleUniverse();
 			BranchGroup group = new BranchGroup();
 			ColorCube cube1 = new ColorCube(0.3);
-			ColorCube cube2 = new ColorCube(0.4);
-			ColorCube cube3 = new ColorCube(0.3);
 			group.addChild(cube1);
-			group.addChild(cube2);
-//			transform.addChild(cube2);
 			universe.getViewingPlatform().setNominalViewingTransform();
 			universe.addBranchGraph(group);
 			
