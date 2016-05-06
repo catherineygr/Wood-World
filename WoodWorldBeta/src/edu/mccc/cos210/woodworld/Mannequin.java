806abd3093d365865957e7e69caa73c568ffc9bd
lemon-ada <catherine.ygr@gmail.com>
2016-05-06 09:05:04 -0400

    new mannequin code

--- WoodWorldBeta/src/edu/mccc/cos210/woodworld/Mannequin.java
+++ WoodWorldBeta/src/edu/mccc/cos210/woodworld/Mannequin.java
@@ -25,14 +25,15 @@ abstract class Mannequin extends TransformGroup {
 	Appearance app;
 	UpperBody upperBody;
 	LowerBody lowerBody;
+	BoundingSphere boundingSphere = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE);
 	String name;
-	double scalex, scaley, scalez;
-	public Mannequin(double scalex, double scaley) {
-		this.scalex=scalex;
-		this.scaley=scaley;
+	float x, y;
+	public Mannequin(float scalex, float scaley, TextureLoader tl) {
+		this.x = scalex;
+		this.y = scaley;
 		setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 		setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-		Appearance appearance = new Appearance();
+		app = new Appearance();
 		Material material = new Material(
 			new Color3f(1.0f, 1.0f, 1.0f),
 			new Color3f(0.0f, 0.0f, 0.0f),
@@ -40,24 +41,24 @@ abstract class Mannequin extends TransformGroup {
 			new Color3f(1.0f, 1.0f, 1.0f),
 			96.0f
 		);
-		appearance.setMaterial(material);
-		TextureLoader textureLoader = new TextureLoader("floor.jpg", null);
+		app.setMaterial(material);
+		TextureLoader textureLoader = tl;
 		Texture texture = textureLoader.getTexture();
 		texture.setBoundaryModeS(Texture.CLAMP);
 		texture.setBoundaryModeT(Texture.CLAMP);
-		appearance.setTexture(texture);
-		Sphere waist= new Sphere(0.2f, Primitive.ENABLE_GEOMETRY_PICKING |
+		app.setTexture(texture);
+		Sphere waist= new Sphere(0.2f * x, Primitive.ENABLE_GEOMETRY_PICKING |
 				Primitive.GENERATE_NORMALS |
 				Primitive.GENERATE_TEXTURE_COORDS,
-				appearance);
-		UpperBody ub = new UpperBody(appearance);
+				app);
+		UpperBody ub = new UpperBody();
 		Transform3D t3dub = new Transform3D();
-		t3dub.setTranslation(new Vector3f(0.0f, 0.4f, 0.0f));
+		t3dub.setTranslation(new Vector3f(0.0f * x, 0.4f * y, 0.0f));
 		ub.setTransform(t3dub);
 		addChild(ub);
-		LowerBody lb = new LowerBody(appearance);
+		LowerBody lb = new LowerBody();
 		Transform3D t3dlb = new Transform3D();
-		t3dlb.setTranslation(new Vector3f(0.0f, -0.25f, 0.0f));
+		t3dlb.setTranslation(new Vector3f(0.0f * x, -0.25f * y, 0.0f));
 		lb.setTransform(t3dlb);
 		addChild(lb);
 		addChild(waist);		
@@ -65,25 +66,24 @@ abstract class Mannequin extends TransformGroup {
 	public class Segment extends TransformGroup{
 		double scalex, scaley;
 		Sphere joint;
-		Appearance app;
-		public Segment(Appearance app) {
-			joint= new Sphere(0.1f, Primitive.ENABLE_GEOMETRY_PICKING |
+		public Segment() {
+			joint= new Sphere(0.1f * x, Primitive.ENABLE_GEOMETRY_PICKING |
 					Primitive.GENERATE_NORMALS |
 					Primitive.GENERATE_TEXTURE_COORDS,
 					app);
-			Cylinder limb = new Cylinder(0.08f, 0.3f, Primitive.ENABLE_GEOMETRY_PICKING |
+			Cylinder limb = new Cylinder(0.08f * x, 0.3f * y, Primitive.ENABLE_GEOMETRY_PICKING |
 					Primitive.GENERATE_NORMALS |
 					Primitive.GENERATE_TEXTURE_COORDS,
 					app);
 			TransformGroup tg = new TransformGroup();
 			tg.addChild(limb);
 			Transform3D t3d = new Transform3D();
-			t3d.setTranslation(new Vector3f(0.0f, -0.2f, 0.0f));
+			t3d.setTranslation(new Vector3f(0.0f * x, -0.2f * y, 0.0f));
 			tg.setTransform(t3d);
 			addChild(joint);
 			addChild(tg);
 		}
-		
+				
 	}
 	public class UpperBody extends TransformGroup {
 		Box torso;
@@ -91,48 +91,29 @@ abstract class Mannequin extends TransformGroup {
 		Arm lArm;
 		Head head;
 		double scalex, scaley;
-		Appearance app;
-		private BoundingSphere boundingSphere = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE);
-		public UpperBody(Appearance app) {
+		public UpperBody() {
 			setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 			setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-			torso = new Box(0.25f, 0.25f, 0.3f, Primitive.ENABLE_GEOMETRY_PICKING |
+			torso = new Box(0.25f * x, 0.25f * y, 0.3f, Primitive.ENABLE_GEOMETRY_PICKING |
 					Primitive.GENERATE_NORMALS |
 					Primitive.GENERATE_TEXTURE_COORDS, app);
 			addChild(torso);
-			rArm= new Arm(app);
-			lArm= new Arm(app);
-			head = new Head(app);
+			rArm= new Arm();
+			lArm= new Arm();
+			head = new Head();
 			Transform3D t3dr= new Transform3D();
+			rArm.getTransform(t3dr);
 			Transform3D t3dl= new Transform3D();
+			lArm.getTransform(t3dl);
 			Transform3D t3dh= new Transform3D();
-			t3dr.setTranslation(new Vector3f(0.35f, 0.15f, 0.0f));
-			t3dl.setTranslation(new Vector3f(-0.35f, 0.15f, 0.0f));
-			t3dh.setTranslation(new Vector3f(0.0f, 0.2f, 0.0f));
+			head.getTransform(t3dh);
+			t3dr.setTranslation(new Vector3f(0.35f * x, 0.15f * y, 0.0f));
+			t3dl.setTranslation(new Vector3f(-0.35f * x, 0.15f * y, 0.0f));
+			t3dh.setTranslation(new Vector3f(0.0f * x, 0.2f * y, 0.0f));
 			rArm.setTransform(t3dr);
 			lArm.setTransform(t3dl);
 			head.setTransform(t3dh);
 			
-			
-//			RotationInterpolator ri = new RotationInterpolator(
-//					new Alpha(
-//						-1,
-//						Alpha.INCREASING_ENABLE,
-//						0,
-//						0,
-//						500,
-//						0,
-//						0,
-//						0,
-//						0,
-//						0
-//					),
-//					this
-//				);
-//			ri.setSchedulingBounds(boundingSphere);
-//			ri.setEnable(false);
-//			Dance dance = new Dance(ri.hashCode(), 2500, ri, this); 
-//			rArm.addChild(ri);	
 			addChild(rArm);
 			addChild(lArm);
 			addChild(head);
@@ -145,19 +126,18 @@ abstract class Mannequin extends TransformGroup {
 		private Leg lleg;
 		Transform3D t3d;
 		double scalex, scaley, scalez;
-		Appearance app;
-		public LowerBody(Appearance app) { 
+		public LowerBody() { 
 			setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 			setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-			pelvis = new Box(0.25f, 0.2f, 0.2f, Primitive.ENABLE_GEOMETRY_PICKING |
+			pelvis = new Box(0.25f * x, 0.2f * y, 0.2f, Primitive.ENABLE_GEOMETRY_PICKING |
 					Primitive.GENERATE_NORMALS |
 					Primitive.GENERATE_TEXTURE_COORDS, app);
-			rleg = new Leg(app);
-			lleg = new Leg(app);
+			rleg = new Leg();
+			lleg = new Leg();
 			Transform3D t3dr= new Transform3D();
 			Transform3D t3dl= new Transform3D();
-			t3dr.setTranslation(new Vector3f(0.15f,-0.3f, 0.0f));
-			t3dl.setTranslation(new Vector3f(-0.15f,-0.3f, 0.0f));
+			t3dr.setTranslation(new Vector3f(0.15f * x,-0.3f * y, 0.0f));
+			t3dl.setTranslation(new Vector3f(-0.15f * x,-0.3f * y, 0.0f));
 			rleg.setTransform(t3dr);
 			lleg.setTransform(t3dl);
 			addChild(pelvis);
@@ -168,52 +148,68 @@ abstract class Mannequin extends TransformGroup {
 	public class Arm extends TransformGroup{
 		private BoundingSphere boundingSphere = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE);
 		double scalex, scaley, scalez;
-		Segment upper, lower, hand;
-		Appearance app;
-		public Arm(Appearance app) {
+		Segment upper, lower;
+		Extreme hand;
+		RotationInterpolator ri;
+		public Arm() {
 			setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 			setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-			this.app=app;
-			upper= new Segment(app);
+			upper= new Segment();
 			
-			lower= new Segment(app);
+			lower= new Segment();
 			Transform3D lt3d = new Transform3D();
-			lt3d.setTranslation(new Vector3f(0.0f, -0.4f, 0.0f));
-			hand= new Segment(app);
+			lt3d.setTranslation(new Vector3f(0.0f * x, -0.4f * y, 0.0f));
+			hand= new Extreme();
 			Transform3D ht3d = new Transform3D();
-			ht3d.setTranslation(new Vector3f(0.0f, -0.8f, 0.0f));
+			ht3d.setTranslation(new Vector3f(0.0f * x, -0.8f * y, 0.0f));
 		
 			lower.setTransform(lt3d);
 			hand.setTransform(ht3d);
+			TransformGroup tg = new TransformGroup();
+			tg.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+			tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+			ri = new RotationInterpolator(
+					new Alpha(
+						-1,
+						Alpha.INCREASING_ENABLE,
+						0,
+						0,
+						500,
+						0,
+						0,
+						0,
+						0,
+						0
+					),
+					tg
+				);
 			
-			addChild(upper);
-			addChild(lower);
-			addChild(hand);
+			ri.setSchedulingBounds(boundingSphere);
+			ri.setEnable(true);
+			
+			tg.addChild(ri);
+			tg.addChild(upper);
+			tg.addChild(lower);
+			tg.addChild(hand);
+			addChild(tg);
 		}
 	}
 	public class Leg extends TransformGroup {
-		public static final int LEFT_LEG = 1;
-		public static final int RIGHT_LEG = 2;
-		double scalex, scaley, scalez;
-		private Segment upper;  
-		private Segment lower;
-		private Segment foot;
-		private Appearance app;
-		public Leg(Appearance app) {
+		private Segment upper, lower;
+		private Extreme foot;
+		public Leg() {
 			setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 			setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-			this.app=app;
-			upper= new Segment(app);
-			
-			lower= new Segment(app);
+			upper= new Segment();
+			lower= new Segment();
 			Transform3D lt3d = new Transform3D();
-			lt3d.setTranslation(new Vector3f(0.0f, -0.4f, 0.0f));
-			foot= new Segment(app);
-			Transform3D ht3d = new Transform3D();
-			ht3d.setTranslation(new Vector3f(0.0f, -0.8f, 0.0f));
-		
+			lt3d.setTranslation(new Vector3f(0.0f * x, -0.4f * y, 0.0f));
+			foot= new Extreme();
+			Transform3D ft3d = new Transform3D();
+			ft3d.setTranslation(new Vector3f(0.0f * x, -0.8f * y, 0.0f));
+			ft3d.rotX(Math.PI/2);
 			lower.setTransform(lt3d);
-			foot.setTransform(ht3d);
+			foot.setTransform(ft3d);
 			
 			addChild(upper);
 			addChild(lower);
@@ -223,20 +219,40 @@ abstract class Mannequin extends TransformGroup {
 	public class Head extends TransformGroup {
 		double scalex, scaley;
 		Sphere joint;
-		Appearance app;
-		public Head(Appearance app) {
-			joint= new Sphere(0.1f, Primitive.ENABLE_GEOMETRY_PICKING |
+		public Head() {
+			joint= new Sphere(0.1f * x, Primitive.ENABLE_GEOMETRY_PICKING |
 					Primitive.GENERATE_NORMALS |
 					Primitive.GENERATE_TEXTURE_COORDS,
 					app);
-			Sphere head = new Sphere(0.2f, Primitive.ENABLE_GEOMETRY_PICKING |
+			Sphere head = new Sphere(0.2f * x, Primitive.ENABLE_GEOMETRY_PICKING |
 					Primitive.GENERATE_NORMALS |
 					Primitive.GENERATE_TEXTURE_COORDS,
 					app);
 			TransformGroup tg = new TransformGroup();
 			tg.addChild(head);
 			Transform3D t3d = new Transform3D();
-			t3d.setTranslation(new Vector3f(0.0f, 0.25f, 0.0f));
+			t3d.setTranslation(new Vector3f(0.0f * x, 0.25f * y, 0.0f));
+			tg.setTransform(t3d);
+			addChild(joint);
+			addChild(tg);
+		}
+	}
+	public class Extreme extends TransformGroup {
+		double scalex, scaley;
+		Sphere joint;
+		public Extreme() {
+			joint= new Sphere(0.1f * x, Primitive.ENABLE_GEOMETRY_PICKING |
+					Primitive.GENERATE_NORMALS |
+					Primitive.GENERATE_TEXTURE_COORDS,
+					app);
+			Box limb = new Box(0.1f * x, 0.1f * y, 0.08f, Primitive.ENABLE_GEOMETRY_PICKING |
+					Primitive.GENERATE_NORMALS |
+					Primitive.GENERATE_TEXTURE_COORDS,
+					app);
+			TransformGroup tg = new TransformGroup();
+			tg.addChild(limb);
+			Transform3D t3d = new Transform3D();
+			t3d.setTranslation(new Vector3f(0.0f * x, -0.2f * y, 0.0f));
 			tg.setTransform(t3d);
 			addChild(joint);
 			addChild(tg);
--- WoodWorldBeta/src/edu/mccc/cos210/woodworld/Manny.java
+++ WoodWorldBeta/src/edu/mccc/cos210/woodworld/Manny.java
@@ -1,13 +1,22 @@
 package edu.mccc.cos210.woodworld;
 
+import java.util.Vector;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Material;
+import javax.media.j3d.Texture;
 import javax.media.j3d.Transform3D;
+import javax.vecmath.Color3f;
 import javax.vecmath.Vector3f;
 
-public class Manny extends Mannequin {
-	//private RotationInterpolator ri;
+import com.sun.j3d.utils.image.TextureLoader;
 
-	public Manny(double scalex, double scaley) {
-		super(scalex, scaley);
+public class Manny extends Mannequin {
+	Appearance app;
+	static TextureLoader tl = new TextureLoader("floor.jpg", null);
+	public Manny() {
+		super(1.5f, 1.0f, tl);
+		
 	}
 	@Override
 	public void dance() {
@@ -19,8 +28,12 @@ public class Manny extends Mannequin {
 	public void jump() {
 		Vector3f vector = new Vector3f(0.0f, 0.5f, 0.0f);
 		Transform3D t3d = new Transform3D();
+		getTransform(t3d);
 		t3d.setTranslation(vector);
+		
+		
 		setTransform(t3d);
+		
 		
 	}
 	@Override
